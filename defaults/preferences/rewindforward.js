1dd5da84bf121073fd33e002f15d5b91d6c068cc
piro <piro@599a83e7-65a4-db11-8015-0010dcdd6dc2>
2007-05-13 20:44:36 +0000

    拡張機能名と説明文をローカライズした
    git-svn-id: https://www.cozmixng.org/repos/piro/rewindforward/trunk@573 599a83e7-65a4-db11-8015-0010dcdd6dc2

---
+++ defaults/preferences/rewindforward.js
@@ -0,0 +1,2 @@
+pref("extensions.{FA4658DE-935B-4f39-AED3-0B5034DDE225}.name", "chrome://rewindforward/locale/rewindforward.properties");
+pref("extensions.{FA4658DE-935B-4f39-AED3-0B5034DDE225}.description", "chrome://rewindforward/locale/rewindforward.properties");
